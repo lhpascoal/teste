4f965d8e91932e3b0bf458b9e6d68c76c16b44cd
Pabarop e <lhpascoal2@gmail.com>
2020-10-11 19:59:08 -0300

    primeiro-commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>App desenvolvido por Luiz Pascoal para fins de estudos</p>
+        <h2>Missão e visão</h2>
+        <p>Aprender backend com Javascript e frontend com React </p>
+        <h2>Contato</h2>
+        <p>lhpascoal2@gmail.com</p>
+    </div>
+)
